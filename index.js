78534d7593dd405b34488fdc7436eb1bfb29a0b9
eunthebear <eun.david@gmail.com>
2017-11-15 07:14:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-class Scooter(year, color, model) {
-  this.year = year;
-  this.color = color;
-  this.model = model;
+class Scooter {
+  constructor (year, color, model) {
+    this.year = year;
+    this.color = color;
+    this.model = model;
+  }
 }
 
-class Driver(name, age, experience) {
-  this.name = name;
-  this.age = age;
-  this.experience = experience;
+class Driver{
+  constructor(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
 }
 
-class PickupLocation(address, city) {
-  this.address = address;
-  this.city = city;
+class PickupLocation {
+  constructor(address, city) {
+    this.address = address;
+    this.city = city;
+  }
 }
